3ff7049cb72bae08cfb84c43d4ab5d869aa41ce6
neurotoxin78 <neuro@abz.kiev.ua>
2015-05-13 13:48:39 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,43 +1,17 @@
-# PyQt4Template. Created on 14.04.2015
-# Copyright (C) 2014 Andreas Schulz <andi.schulz@me.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 US
+# -*- coding: utf-8 -*-
 
+from PyQt4 import QtCore, QtGui, uic
 import sys
-from os import path
-import sip
-sip.setapi('QString', 2)
-sip.setapi('QVariant', 2)
 
-from PyQt4.QtGui import QApplication
-
-thisfile = path.abspath(__file__)
-sys.path.insert(0, path.normpath(path.join(path.dirname(thisfile), '..')))
-
-from widgets.mainwindow import MainWindow
-
-
-def main(argv=None):
-    if not argv:
-        argv = sys.argv
-
-    app = QApplication(argv)
-    window = MainWindow()
+def main ():
+    """ Function doc """    
+    app = QtGui.QApplication(sys.argv)
+    window = uic.loadUi("ui/mainwindow.ui") 
+    #QtCore.QObject.connect(window.pushButton,QtCore.SIGNAL("clicked()"),generate)
+    
+    
     window.show()
-    return app.exec_()
-
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+	main()
